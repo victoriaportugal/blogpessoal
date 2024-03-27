7af664a370c7e2c8a5f0c02b5913e50c02b37988
victoriaportugal <victoria.poortugal@gmail.com>
2024-03-27 14:55:29 -0300

    Colocando cadastrar, atualizar, deletar...

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,16 +1,27 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/postagens")
@@ -24,5 +35,55 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> getAll(){
 		return ResponseEntity.ok(postagemRepository.findAll());
 	}
+	@GetMapping("/titulo/{titulo}")
+    public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+        return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+        // ATENÇÃO: Por que aqui a gente não fez um map ,else ? Todo método que tem como
+        // retorno
+        // uma lista,auomaticamente ela será gerada,então caso ele não encontre ainda
+        // assim será
+        // retornado uma lista vazia
+        //Ao encapsular a lista de postagens dentro de ResponseEntity.ok(...),
+        // você está indicando explicitamente que a operação
+        // foi bem-sucedida (código 200 OK) e está incluindo o corpo da resposta, que é
+        // a lista de postagens.
+    }
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				//.map serve para verificar enquanto nao é nulo
+				//.ok rerifica se a resposta é realizada
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(postagemRepository.save(postagem));
+	}
+	@PutMapping 
+	public ResponseEntity <Postagem> put(@Valid @RequestBody Postagem postagem){
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+    // Eu estou falando que caso a requisição aconteça corretamente,ele retorna um
+    // HTTP No_content(204)
+    // ao invés de retornar OK(200)
+    @DeleteMapping("/{id}")
+    // passando o id da publicação que eu quero apagar
+    public void delete(@PathVariable Long id) {
+        Optional<Postagem> postagem = postagemRepository.findById(id);
+        // Caso não seja encontrado a postagem com o id informado,graças ao Optional ele
+        // irá encapsular a resposta do Método findById(id)
+        if (postagem.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
 
+        postagemRepository.deleteById(id);
+        // Aqui ele está removendo,retorna uma NO_CONTENT 204
+
+}
 }
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,22 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
 
 }
+
+//Criando uma lista pra armazenar todos as postagens;
+//Find : SELECT
+//by : WHERE
+//Titulo : Atributo da classe postagem
+//Containing : Like
+//IgnoreCase: Ignorando letras maiúsculas ou minúsculas
+//@Param("titulo") : Define a variável String titulo como um parâmetro da consulta. Esta anotação é obrigatório em consultas do tipo Like.
