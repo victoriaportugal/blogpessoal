2ef1de5c7a311d2152e91e7358be2b227de5dadc
victoriaportugal <victoria.poortugal@gmail.com>
2024-04-11 16:53:01 -0300

    Deply do Projeto Blog Pessoal

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -36,8 +37,9 @@ public class Usuario {
 	@NotNull(message = "O Atributo Nome é obrigatório")
 	private String nome;
 	
-	@NotNull(message = "O Atributo Email é obrigatório")
-	@Email(message = "O Atributo usuario deve ser um email valido")
+	@Schema(example = "email@email.com.br")
+	@NotNull(message = "O Atributo Usuário é Obrigatório!")
+	@Email(message = "O Atributo Usuário deve ser um email válido!")
 	private String usuario;
 	
 	@NotBlank(message = "O atributo senha é obrigatório")
